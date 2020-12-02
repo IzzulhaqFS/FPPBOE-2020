bad3efce9299e8d190b1d20632c4b568e0fafa7c
Izzulhaq Fawwaz Syauqiy <57482751+IzzulhaqFS@users.noreply.github.com>
2020-12-02 16:15:26 +0700

    Add files via upload

---
+++ ShapeGUISimpleIO/src/id/ac/its/izzulhaq023/Shape2D.java
@@ -0,0 +1,12 @@
+package id.ac.its.izzulhaq023;
+
+public abstract class Shape2D {
+
+	public Shape2D() {
+		
+	}
+	
+	public abstract double getArea();
+	
+	public abstract double getCircumference();
+}
